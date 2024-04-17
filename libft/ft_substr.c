fee021df32715381425031c425d745bf21e1a4a7
mikeylucca <134014129+mikeylucca@users.noreply.github.com>
2024-04-17 19:38:53 +0000

    update on substr to return NULL instead of 0

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 19:26:19 by misoares          #+#    #+#             */
-/*   Updated: 2024/04/17 19:34:01 by misoares         ###   ########.fr       */
+/*   Updated: 2024/04/17 19:37:28 by misoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
     char *ret;
 
     if (!s)
-        return (0);
+        return (NULL);
     if (ft_strlen(s) < start)
         len = 0;
     if (ft_strlen(s + start) < len)
         len = ft_strlen(s + start);
     ret = malloc(sizeof(char) * (len + 1));
     if (!ret)
-        return (0);
+        return (NULL);
     ft_strlcpy(ret, s + start, len + 1);
     return (ret);
 }
