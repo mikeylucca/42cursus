80a4ca40325e5b354667db102c03e130aacba64c
mikeylucca <134014129+mikeylucca@users.noreply.github.com>
2024-04-17 20:07:06 +0000

    strjoin correct later

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 19:40:09 by misoares          #+#    #+#             */
+/*   Updated: 2024/04/17 19:59:28 by misoares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    int i;
+    int len1;
+    int len2;
+    char *str;
+
+    if (s1 && s2)
+    {
+            len1 = ft_strlen(s1);
+            len2 = ft_strlen(s2);
+            str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+            if (str == NULL)
+             return (NULL);
+            i = -1;
+            while (s1[++i])
+                str[i] = s2[i];
+            i = -1;
+            while (s2[++i])
+            {
+                str[len1] = s2[i];
+                len1++;
+            }
+            str[len1] = '\0';
+            return (str);
+    }
+    else
+        return (NULL);
+}
