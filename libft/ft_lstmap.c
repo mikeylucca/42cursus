f33764ef53f1eeee217956f243e5dd7c1d4473fa
MikeLaptopLinux <mike.souza2k19@gmail.com>
2024-04-27 15:11:17 +0100

    done falta norm

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,13 +6,33 @@
 /*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 15:56:20 by misoares          #+#    #+#             */
-/*   Updated: 2024/04/21 16:07:13 by misoares         ###   ########.fr       */
+/*   Updated: 2024/04/27 14:50:13 by misoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*set;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		set = f(lst->content);
+		new_node = ft_lstnew(set);
+		if (!new_node)
+		{
+			del(set);
+			ft_lstclear(&new_list, (*del));
+			return (new_list);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
