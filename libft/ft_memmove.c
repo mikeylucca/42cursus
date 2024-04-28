3d0c9b849edeb2268b2cdbee039cd284ac338060
Micael Lucca Soares Souza <misoares@c1r5s3.42lisboa.com>
2024-04-28 19:39:27 +0100

    few corrected

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,14 @@
 /*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:03:50 by misoares          #+#    #+#             */
-/*   Updated: 2024/04/27 17:11:23 by misoares         ###   ########.fr       */
+/*   Updated: 2024/04/28 15:51:37 by misoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/* #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>  */
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -18,18 +21,48 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*sr;
 	size_t			i;
 
-	i = -1;
 	ds = (unsigned char *)dest;
 	sr = (unsigned char *)src;
 	if (!sr && !ds)
 		return (NULL);
 	if (sr < ds)
 	{
-		while (++i <= n)
-			ds[n - i] = sr[n - i];
+		i = n;
+		while (i > 0)
+		{
+			ds[i - 1] = sr[i - 1];
+			i--;
+		}
 	}
 	else
-		while (n-- > 0)
-			*(ds++) = *(sr++);
-	return (ds);
+	{
+		i = -1;
+		while (++i < n)
+			ds[i] = sr[i];
+	}
+	return (dest);
 }
+
+/* int main(void)
+{
+	const char *string = "Test string";
+	const char *string2 = "Test string";
+
+	char *src = (char *)malloc(strlen(string) + 10);
+	char *src2 = (char *)malloc(strlen(string2) + 1);
+
+	strcpy(src, string);
+	strcpy(src2, string2);
+
+	char dest[50];
+	char dest2[50];
+
+	memmove(src2, src2 + 3, strlen(src2 + 3) + 1);
+	printf("original memmove: |%s|\n", src2);
+
+	ft_memmove(src, src + 3, strlen(src + 3) + 1);
+	printf("ft_memmove: |%s|\n", src);
+
+	free(src);
+	free(src2);
+} */
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,24 +6,38 @@
 /*   By: misoares <misoares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:11:27 by misoares          #+#    #+#             */
-/*   Updated: 2024/04/27 15:13:21 by misoares         ###   ########.fr       */
+/*   Updated: 2024/04/28 19:33:49 by misoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/* #include <stdio.h> */
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
+	unsigned char	u1;
+	unsigned char	u2;
 
 	i = 0;
 	while ((s1[i] || s2[i]) && (i < n))
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			u1 = (unsigned char)s1[i];
+			u2 = (unsigned char)s2[i];
+			return (u1 - u2);
 		}
 		i++;
 	}
 	return (0);
 }
+
+/* int main(void)
+{
+	char *str = "string test";
+	char *str2 = "string tesu";
+
+	printf("original strncmp: %i\n", strncmp(str, str2, 11));
+	printf("my strncmp: %i\n", ft_strncmp(str, str2, 11));
+} */
